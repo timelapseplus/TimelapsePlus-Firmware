365fcd8eb1e9b6111724ade881294908cab1a647
Elijah Parker <eparker@cws.net>
2012-10-06 21:40:01 -0500

    increased the height of the 3 in the big font

--- src/Menu_Map.h
+++ src/Menu_Map.h
@@ -213,7 +213,7 @@ const menu_item menu_settings[]PROGMEM =
 const menu_item menu_main[]PROGMEM =
 {
     { "Trigger     ", 'M', (void*)menu_trigger, 0, (void*)&timerNotRunning },
-    { "Timelapse   ", 'M', (void*)menu_timelapse, 0, (void*)&timerQuickNotRunning },
+    { "Timelapse   ", 'M', (void*)menu_timelapse, 0, 0 },
     { "Connect     ", 'M', (void*)menu_connect, 0, 0 },
     { "Settings    ", 'M', (void*)menu_settings, 0, 0 },
     { "Power Off   ", 'F', (void*)hardware_off, 0, 0 },
