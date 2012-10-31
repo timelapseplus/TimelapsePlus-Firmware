5e2388d72cb94ffdc5d269fb85f24692976add71
Max Bäumle <maxbaeumle@Maxs-MacBook-Air.local>
2012-10-31 13:48:57 +0100

    Update battery every minute (finally)

--- src/timelapseplus.cpp
+++ src/timelapseplus.cpp
@@ -278,13 +278,19 @@ int main()
 
 		clock.sleepOk = timerNotRunning && !timer.cableIsConnected() && bt.state != BT_ST_CONNECTED;
 
-		uint8_t batteryRead = battery_read();
+		static uint32_t startTime = 0;
 
-		if(batteryRead != battery_percent && (batteryRead < battery_percent - 1 || batteryRead > battery_percent + 1 || batteryRead > 99))
+		if(clock.Ms() > startTime + 60000)
 		{
-			battery_percent = batteryRead;
+			startTime = clock.Ms();
+			uint8_t batteryRead = battery_read();
 
-			if(remote.notifyBattery) remote.send(REMOTE_BATTERY, REMOTE_TYPE_SEND);
+			if(batteryRead != battery_percent)
+			{
+				battery_percent = batteryRead;
+
+				if(remote.notifyBattery) remote.send(REMOTE_BATTERY, REMOTE_TYPE_SEND);
+			}
 		}
 
 		if((hardware_USB_HostConnected || connectUSBcamera) && !hardware_USB_InHostMode)
