9b735e1b5878525da1df74b82578bdd8f10004e9
Elijah Parker <elijahparker@gmail.com>
2013-01-22 10:58:43 -0600

    Code organization

--- src/debug.cpp
+++ src/debug.cpp
@@ -13,76 +13,9 @@
 #include "timelapseplus.h"
 #include "tlp_menu_functions.h"
 #include "settings.h"
+#include "tldefs.h"
 
 extern settings conf;
-
-/******************************************************************
- *
- *   debug
- *
- *
- ******************************************************************/
-
-void debug(char c)
-{
-    if(conf.devMode == 0) return;
-    VirtualSerial_PutChar(c);
-}
-
-/******************************************************************
- *
- *   debug
- *
- *
- ******************************************************************/
-
-void debug(uint8_t c)
-{
-    if(conf.devMode == 0) return;
-    char buf[5];
-    
-    int_to_str((uint16_t)c, buf);
-    VirtualSerial_PutString(buf);
-}
-
-/******************************************************************
- *
- *   debug
- *
- *
- ******************************************************************/
-
-void debug(uint16_t n)
-{
-    if(conf.devMode == 0) return;
-    char buf[6];
-
-    int_to_str(n, buf);
-    VirtualSerial_PutString(buf);
-}
-
-/******************************************************************
- *
- *   debug
- *
- *
- ******************************************************************/
-
-void debug(uint32_t n)
-{
-    if(conf.devMode == 0) return;
-
-    uint8_t i = 0;
-    char buf[10];
-    while(n)
-    {
-        buf[i] = (char)(n % 10) + '0';
-        n -= n % 10;
-        n /= 10;
-        i++;
-    }
-    while(i--) VirtualSerial_PutChar(buf[i - 1]);
-}
 
 /******************************************************************
  *
@@ -104,10 +37,81 @@ void debug(char *s)
  *
  ******************************************************************/
 
+void debug(char c)
+{
+    if(conf.devMode == 0) return;
+    char buf[2];
+    buf[0] = c;
+    buf[1] = '\0';    
+    debug((char*)buf);
+}
+
+/******************************************************************
+ *
+ *   debug
+ *
+ *
+ ******************************************************************/
+
+void debug(uint8_t c)
+{
+    if(conf.devMode == 0) return;
+    char buf[5];
+    
+    int_to_str((uint16_t)c, buf);
+    debug((char*)buf);
+}
+
+/******************************************************************
+ *
+ *   debug
+ *
+ *
+ ******************************************************************/
+
+void debug(uint16_t n)
+{
+    if(conf.devMode == 0) return;
+    char buf[6];
+
+    int_to_str(n, buf);
+    debug((char*)buf);
+}
+
+/******************************************************************
+ *
+ *   debug
+ *
+ *
+ ******************************************************************/
+
+void debug(uint32_t n)
+{
+    if(conf.devMode == 0) return;
+
+    uint8_t i = 9;
+    char buf[10];
+    buf[9] = '\0';
+    while(n)
+    {
+        i--;
+        buf[i] = (char)(n % 10) + '0';
+        n -= n % 10;
+        n /= 10;
+    }
+    while(i--) debug((char*)&buf[i]);
+}
+
+/******************************************************************
+ *
+ *   debug
+ *
+ *
+ ******************************************************************/
+
 void debug_nl()
 {
     if(conf.devMode == 0) return;
-    VirtualSerial_PutChar('\r');
-    VirtualSerial_PutChar('\n');
+    debug((char*)STR("\r\n"));
 }
 
