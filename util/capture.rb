82c3cf9a6902d11287ae82d80f579c3831e369b0
Elijah Parker <elijahparker@gmail.com>
2013-02-16 10:22:24 -0600

    Ruby script to take a take a picture from the computer over USB

---
+++ util/capture.rb
@@ -0,0 +1,86 @@
+# capture.rb
+# Tells the Timelapse+ to have the camera take a picture
+#
+# Usage:
+#
+# ruby ./capture.rb
+#
+#
+# By Elijah Parker
+# mail@timelapseplus.com
+#
+# For Mac OS X only
+
+
+
+require 'serialport'
+
+
+class TLP
+	def open(port)
+		port_str = port
+		baud_rate = 9600
+		data_bits = 8
+		stop_bits = 1
+		parity = SerialPort::NONE
+		@sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+		@sp.read_timeout=1000
+	end
+
+	def id
+		@sp.putc('T')
+		@sp.getc
+	end
+
+	def capture
+		@sp.putc('C')
+		@sp.getc
+	end
+
+	def screen
+		scr = Array.new()
+		@sp.putc('S')
+		504.times do
+			scr.push(@sp.getbyte)
+		end
+		return scr
+	end
+
+	def close
+		@sp.close if(@sp)
+	end
+
+	def find
+		result = false
+		[1..10].each do
+			list = `ls /dev/tty.usb*`
+			sid = "E"
+			list.split("\n").each do |dev|
+				dev.strip!
+				begin
+					puts "Trying '" + dev + "'..."
+					open(dev)
+					tid = id
+					result = true if tid == sid
+					break
+					puts "Invalid ID (" + tid + ") - expected " + sid + ".\n"
+					close
+				rescue
+					puts "Error opening.\n"
+					close
+					result = false
+				end
+			end
+		end
+		return result
+	end
+end
+
+device = TLP.new
+if(device.find)
+	device.capture
+end
+
+device.close
+
+
